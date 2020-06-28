b63f1e9d1deab2d7a30bd487db79b3354d6c2d9f
pRose on spring <prose@rossroberts.com>
2020-06-28 08:24:18 -0700

    reformat

--- includes/languages/english/modules/payment/authorizenet_cim.php
+++ includes/languages/english/modules/payment/authorizenet_cim.php
@@ -15,11 +15,11 @@ define('MODULE_PAYMENT_AUTHORIZENET_CIM_TEXT_ADMIN_TITLE',
 
 if (defined('MODULE_PAYMENT_AUTHORIZENET_CIM_STATUS') and MODULE_PAYMENT_AUTHORIZENET_CIM_STATUS == 'True') {
     define('MODULE_PAYMENT_AUTHORIZENET_CIM_TEXT_DESCRIPTION',
-        '<br /><br /><a target="_blank" href="https://account.authorize.net/">Authorize.net Merchant Login</a>' . (MODULE_PAYMENT_AUTHORIZENET_CIM_TESTMODE != 'Production' ? '<br /><br />Testing Info:<br /><b>Automatic Approval Credit Card Numbers:</b><br />Visa#: 4007000000027<br />MC#: 5424000000000015<br />Discover#: 6011000000000012<br />AMEX#: 370000000000002<br /><br /><b>Note:</b> These credit card numbers will return a decline in live mode, and an approval in test mode.  Any future date can be used for the expiration date and any 3 or 4 (AMEX) digit number can be used for the CVV Code.<br /><br /><b>Automatic Decline Credit Card Number:</b><br /><br />Card #: 4222222222222<br /><br />This card number can be used to receive decline notices for testing purposes.<br /><br />' : ''));
+        '<br><br><a target="_blank" href="https://account.authorize.net/">Authorize.net Merchant Login</a>' . (MODULE_PAYMENT_AUTHORIZENET_CIM_TESTMODE != 'Production' ? '<br><br>Testing Info:<br><b>Automatic Approval Credit Card Numbers:</b><br >Visa#: 4007000000027<br>MC#: 5424000000000015<br>Discover#: 6011000000000012<br>AMEX#: 370000000000002<br><br><b>Note:</b> These credit card numbers will return a decline in live mode, and an approval in test mode.  Any future date can be used for the expiration date and any 3 or 4 (AMEX) digit number can be used for the CVV Code.<br><br><b>Automatic Decline Credit Card Number:</b><br><br>Card #: 4222222222222<br><br>This card number can be used to receive decline notices for testing purposes.<br><br>' : ''));
 } else {
     define('MODULE_PAYMENT_AUTHORIZENET_CIM_STATUS', false);
     define('MODULE_PAYMENT_AUTHORIZENET_CIM_TEXT_DESCRIPTION',
-        '<br /><strong>PLEASE, PLEASE, PLEASE </strong>use the below link to create your NEW merchant account.  You can keep your transactions separate from any previous authorize.net module as well as benefit me and the ZC team.<br><br><div class="alert alert-info"><a target="_blank" href="http://reseller.authorize.net/application/?resellerId=111066">Click Here to Sign Up for an Account</a><br /><br /><a target="_blank" href="https://account.authorize.net/">Authorize.net Merchant Area</a><br /><br /><strong>Requirements:</strong><hr />*<strong>Authorize.net Account</strong> (use link above to signup)<br />*<strong>CURL is required </strong>and MUST be compiled with SSL support into PHP by your hosting company<br />*<strong>Authorize.net api login ID and transaction key</strong> are available from your Merchant Area</div>');
+        '<hr><strong>PLEASE, PLEASE, PLEASE </strong>use the below link to create your NEW merchant account.  You can keep your transactions separate from any previous authorize.net module as well as benefit me and the ZC team.<br><br><div class="alert alert-info"><a target="_blank" href="http://reseller.authorize.net/application/?resellerId=111066">Click Here to Sign Up for a NEW Merchant Account</a><br><br><a target="_blank" href="https://account.authorize.net/">Authorize.net Merchant Area</a><br><br><strong>Requirements:</strong><hr>*<strong>Authorize.net Account</strong> (use link above to signup)<br>*<strong>CURL is required </strong>and MUST be compiled with SSL support into PHP by your hosting company<br>*<strong>Authorize.net api login ID and transaction key</strong> are available from your Merchant Area</div>');
 }
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_TEXT_ERROR_CURL_NOT_FOUND',
     'CURL functions not found - required for Authorize.net CIM payment module');
@@ -54,7 +54,7 @@ define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_REFUND_TRANS_ID', 'Enter the origi
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_REFUND_TEXT_COMMENTS', 'Notes (will show on Order History):');
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_REFUND_DEFAULT_MESSAGE', 'Refund Issued');
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_REFUND_SUFFIX',
-    'You may refund an order up to the amount already captured. You must supply the last 4 digits of the credit card number used on the initial order.<br />Refunds must be issued within 120 days of the original transaction date.');
+    'You may refund an order up to the amount already captured. You must supply the last 4 digits of the credit card number used on the initial order.<br>Refunds must be issued within 120 days of the original transaction date.');
 
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_CAPTURE_TITLE', '<strong>Capture Transactions</strong>');
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_CAPTURE', 'You may capture previously-authorized funds here:');
@@ -64,7 +64,7 @@ define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_CAPTURE_TRANS_ID', 'Enter the orig
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_CAPTURE_TEXT_COMMENTS', 'Notes (will show on Order History):');
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_CAPTURE_DEFAULT_MESSAGE', 'Settled previously-authorized funds.');
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_CAPTURE_SUFFIX',
-    'Captures must be performed within 30 days of the original authorization. You may only capture an order ONCE. <br />Please be sure the amount specified is correct.<br />If you leave the amount blank, the original amount will be used instead.');
+    'Captures must be performed within 30 days of the original authorization. You may only capture an order ONCE. <br>Please be sure the amount specified is correct.<br>If you leave the amount blank, the original amount will be used instead.');
 
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_VOID_TITLE', '<strong>Voiding Transactions</strong>');
 define('MODULE_PAYMENT_AUTHORIZENET_CIM_ENTRY_VOID', 'You may void a transaction which has not yet been settled:');
