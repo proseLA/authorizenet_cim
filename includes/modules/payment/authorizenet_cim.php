608668dba100285653f4cf45d47f4357383775f0
pRose on charmes <prose@rossroberts.com>
2022-01-06 08:08:38 -0800

    remove stray apple pay button

--- includes/modules/payment/authorizenet_cim.php
+++ includes/modules/payment/authorizenet_cim.php
@@ -160,9 +160,9 @@
                 'id' => $this->code,
                 'module' => MODULE_PAYMENT_AUTHORIZENET_CIM_TEXT_CATALOG_TITLE,
                 'fields' => [
-                    [
+/*                    [
                         'field' => '<div class="apple-pay-button apple-pay-button-white"></div>',
-                    ],
+                    ],*/
                     [
                         'title' => MODULE_PAYMENT_AUTHORIZENET_CIM_TEXT_CREDIT_CARD_OWNER,
                         'field' => zen_draw_input_field('authorizenet_cim_cc_owner',
